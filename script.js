eb0b86e386ba72d1455b25e21d4c5ce40529395d
Zachary Jordan <zachary.jordan@my.whccd.edu>
2025-10-29 22:36:02 -0700

    problem 1 completed

--- script.js
+++ script.js
@@ -3,6 +3,24 @@
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
+//the goal logic -> set a value to really high, i'll use 1000. if num1 is lower than that value, set value to num1. if num2 is lower than that value, set to that value. return lowest value
+
+const min = (num1, num2) => {
+    let lowestValue = 1000; //i know theres a way to set this value to basically infinite, but i didnt want to look that up.
+
+    if (num1 < lowestValue) {
+        lowestValue = num1;
+    }
+
+    if (num2 < lowestValue) {
+        lowestValue = num2;
+    }
+
+    return lowestValue
+};
+
+console.log(min(4,299));
+
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
